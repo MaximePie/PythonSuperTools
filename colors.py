5117566804423dd89c67afa50c529a053baa6def
MaximePie <maxime.pie.mail@gmail.com>
2024-11-27 09:19:32 +0100

    feat: add colors

---
+++ colors.py
@@ -0,0 +1,33 @@
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print("Boring normal text !")
+
+
+# print("\033[91mWarning, this next potato is too hot\033[0m")
+
+
+# print("\033[92mWell anyway, cats rule\033[0m")
+
+
+# print("\033[93mI'm a banana\033[0m")
+
+
+# print("\033[94mI'm blue\033[0m da ba dee da ba daa")
+
+
+
+
+
+
+# Code	Color
+# [30m	Black Text
+# [31m	Red Text
+# [32m	Green Text
+# [33m	Yellow Text
+# [34m	Blue Text
+# [35m	Purple Text
+# [36m	Cyan Text
+# [37m	White Text
